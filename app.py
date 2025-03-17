5597eac968cae17acc5ab5904f31db4dae0bdf4c
SoumojitBhuin <soumojitbhuin2004@gmail.com>
2025-03-17 22:50:13 +0530

    Updated requirements for Vercel

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
@@ -10,7 +9,8 @@ load_dotenv()
 app = Flask(__name__)
 
 # Database Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+db_path = os.path.join("/tmp", "database.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
@@ -30,37 +30,3 @@ register_routes(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
-import yaml
-
-load_dotenv()
-
-app = Flask(__name__)
-
-# Database Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
-# Importing Models and Initializing Database
-from appfile.models import db
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
-
-# Importing Routes
-from routes import register_routes
-register_routes(app)
-
-
-
-
-if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
->>>>>>> Stashed changes
